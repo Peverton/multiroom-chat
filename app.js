8fc9d7b0d0bf39f0f25996dc6208cce97aa178ec
Peverton <pedro.ev14@hotmail.com>
2019-09-20 23:46:46 -0300

    Application built using Socket.io

---
+++ app.js
@@ -0,0 +1,30 @@
+const app = require('./config/server');
+
+var server = app.listen(80, function() {
+  console.log("Servidor online");
+});
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+// Criar conexão por websocket
+io.on('connection', function(socket) {
+  console.log("Usuário conectou");
+
+  socket.on('disconnect', function() {
+    console.log("Usuário desconectou");
+  });
+
+  socket.on('msgParaServidor', function(data) {
+    //dialogo
+    socket.emit('msgParaCliente', { apelido: data.apelido, mensagem: data.mensagem });
+    socket.broadcast.emit('msgParaCliente', { apelido: data.apelido, mensagem: data.mensagem });
+
+    // participante
+    if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+      socket.emit('participantesParaCliente', { apelido: data.apelido });
+      socket.broadcast.emit('participantesParaCliente', { apelido: data.apelido });
+    }
+  });
+});
